49fc63ab0e9ca3056e4714d0a3a13b12f7cd4c78
Jesse Gould <jesse.gould@codeimmersives.com>
2019-11-21 16:00:36 -0500

    before weekend

---
+++ main.js
@@ -0,0 +1,14 @@
+//functions
+
+function bigButton(){
+document.querySelector('#user-input').event.value
+}
+
+
+
+
+
+
+//Event Selectors
+document.querySelector('.biggify').addEventListener(click, bigButton);
+let inputText = document.querySelector('#user-input').addEventListener(KeyboardEvent,fnc)  
