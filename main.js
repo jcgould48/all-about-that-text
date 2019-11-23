8cc5f3d76c7a0f3d585d8e28b055e878fb8abc88
Jesse Gould <jesse.gould@codeimmersives.com>
2019-11-22 20:55:13 -0500

    makes function to a variable

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 //functions
 
-    
+// const inputText = document.querySelector('#user-input').value   
 
-function bigButton(){
+let bigButton = function (){
 let inputText = document.querySelector('#user-input').value
 let number = Number(inputText);
 document.querySelector('.result').innerText = biggify(number);
@@ -16,4 +16,4 @@ document.querySelector('.result').innerText = biggify(number);
 //Event Selectors
 
 document.querySelector('.biggify').addEventListener('click', bigButton);
-// let inputText = document.querySelector('#user-input').addEventListener(KeyboardEvent,fnc)  
+  
