c6e56e41e3760f77437880be0b1e3d5c1cfca442
Jesse Gould <jesse.gould@codeimmersives.com>
2019-11-24 10:25:39 -0500

    removes search query

--- main.js
+++ main.js
@@ -6,30 +6,35 @@ const bigButton = function (){
 let inputText = document.querySelector('#user-input').value
 let number = Number(inputText);
 document.querySelector('.result').innerText = biggify(number);
+document.querySelector('#user-input').value=''
 }
 
 const nasaBtn = function (){
 let inputText = document.querySelector('#user-input').value
 let number = Number(inputText);
 document.querySelector('.result').innerText = nasafy(number);
+document.querySelector('#user-input').value=''
 }
 
 const crazyBtn = function (){
 let inputText = document.querySelector('#user-input').value
 
 document.querySelector('.result').innerText = crazify(inputText);
+document.querySelector('#user-input').value=''
 }
 
 const reverseBtn = function (){
 let inputText = document.querySelector('#user-input').value
 
 document.querySelector('.result').innerText = reversify(inputText);
+document.querySelector('#user-input').value=''
 }
 
 const titleBtn = function (){
 let inputText = document.querySelector('#user-input').value
 
 document.querySelector('.result').innerText = titleify(inputText);
+document.querySelector('#user-input').value=''
 }
 
 
