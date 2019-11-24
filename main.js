364c00905f8102d254293029f739781409e4ee10
Jesse Gould <jesse.gould@codeimmersives.com>
2019-11-24 10:18:12 -0500

    adds other formulas

--- main.js
+++ main.js
@@ -2,10 +2,34 @@
 
 // const inputText = document.querySelector('#user-input').value   
 
-let bigButton = function (){
+const bigButton = function (){
 let inputText = document.querySelector('#user-input').value
 let number = Number(inputText);
 document.querySelector('.result').innerText = biggify(number);
+}
+
+const nasaBtn = function (){
+let inputText = document.querySelector('#user-input').value
+let number = Number(inputText);
+document.querySelector('.result').innerText = nasafy(number);
+}
+
+const crazyBtn = function (){
+let inputText = document.querySelector('#user-input').value
+
+document.querySelector('.result').innerText = crazify(inputText);
+}
+
+const reverseBtn = function (){
+let inputText = document.querySelector('#user-input').value
+
+document.querySelector('.result').innerText = reversify(inputText);
+}
+
+const titleBtn = function (){
+let inputText = document.querySelector('#user-input').value
+
+document.querySelector('.result').innerText = titleify(inputText);
 }
 
 
@@ -16,4 +40,7 @@ document.querySelector('.result').innerText = biggify(number);
 //Event Selectors
 
 document.querySelector('.biggify').addEventListener('click', bigButton);
-  
+document.querySelector('.nasafy').addEventListener('click', nasaBtn);
+document.querySelector('.crazify').addEventListener('click', crazyBtn);
+document.querySelector('.reversify').addEventListener('click', reverseBtn);
+document.querySelector('.titleify').addEventListener('click', titleBtn);
